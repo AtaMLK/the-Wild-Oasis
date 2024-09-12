430a4fa1ccd9152e3fbe1a0cb2236e8bade619c2
Ata molkarae <molkaraeeata@gmail.com>
2024-09-12 23:15:17 +0300

    adding sort option to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -10,18 +10,18 @@ import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 
 /* const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`; */
+    display: grid;
+    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+    column-gap: 2.4rem;
+    align-items: center;
+    background-color: var(--color-grey-50);
+    border-bottom: 1px solid var(--color-grey-100);
+    text-transform: uppercase;
+    letter-spacing: 0.4px;
+    font-weight: 600;
+    color: var(--color-grey-600);
+    padding: 1.6rem 2.4rem;
+  `; */
 
 function CabinTable() {
   const { isLoading, cabins, error } = UseCabins();
@@ -29,17 +29,30 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
-  const filtereValue = searchParams.get("discount") || "all";
+  // 1) FILTER
+  const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
 
-  if (filtereValue === "all") filteredCabins = cabins;
+  if (filterValue === "all") filteredCabins = cabins;
 
-  if (filtereValue === "no-discount")
+  if (filterValue === "no-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
 
-  if (filtereValue === "with-discount")
+  if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 1) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+
+  const [field, direction] = sortBy.split("-");
+
+  const modifier = direction === "asc" ? 1 : -1;
+
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+  console.log(sortBy);
 
   return (
     <Menus>
@@ -53,8 +66,9 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          /*           data={cabins}
-           */ data={filteredCabins}
+          /*data={cabins}*/
+          /*  data={filteredCabins} */
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
