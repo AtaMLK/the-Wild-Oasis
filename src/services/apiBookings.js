6a016f6c1bfae4a23ac46737102a82bf05142382
Ata molkarae <molkaraeeata@gmail.com>
2024-09-24 07:25:59 +0300

    Booking sort - filter finished

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,24 @@
+/* eslint-disable no-unused-vars */
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id , created_at , startDate, endDate , numNights,numGuests , status,totalPrice, isPaid,cabins(name),guests(fullName)"
     );
+
+  //FILTER
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  //SORT
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
